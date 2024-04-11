4fe4b9334e48caa29e87bc6041805451d0284500
Gaurav Kumar Singh <113422679+Gauravk825@users.noreply.github.com>
2024-04-11 10:24:59 +0530

    Add files via upload

---
+++ Assignment2_Q1.py
@@ -0,0 +1,4 @@
+add = 0 # intialization of add variable for addition
+for i in range(10):
+    add += int(input("Enter 10 integers:")) # input from use and adding simultaneously 
+print(f"Avraeg of number {add/10}")
